7586e3eab9c192e8793d8c01b7f4e3fa0e6c108b
Hyorm <21600193@handong.edu>
2017-10-05 12:19:03 +0900

    edit1 C++

--- C++/HW4_21600193_KimHyoRim.cpp
+++ C++/HW4_21600193_KimHyoRim.cpp
@@ -5,15 +5,6 @@
 
 using namespace std;
 
-book* load();
-void save(string fileName);
-void print(book[] bookList);
-book insert(string[] arr);
-book* lend(book[] bookList);
-book* passDay(book[] bookList);
-book returned(book r_book);
-int getExit();
-
 //DONE: ONE - 	make
 struct book{
 
@@ -21,32 +12,123 @@ struct book{
 	string p_Year;
 	string author;
 	string edition;
-	string b_Person;
-	int l_Day; 
+	string b_Person = "None";
+	int l_Day = 0; 
 
 };
+
+void printMenu();
+
+book *load();
+void save(string fileName);
+void print(book[] bookList);
+book insert(string[] arr);
+book *lend(book[] bookList);
+book *passDay(book[] bookList);
+book returned(book r_book);
+int getExit();
 
 int main(){
 
 	int setExit = 1;
 
-	book[50] bookList;
+	book b_List[50];
 
-	while(exit){
+	string openFile = "first.txt";
 
-	
-		setExit = getExit();
+	while(setExit){
 
+		b_List = load(openFile);
+
+		string argLine, arg[4];
+
+		enum menu {INSERT, Insert, insert, LEND, Lend, lend, SAVE, Save, save, RETURNED, Returned, returned, PASSDAY, Passday, passday, PRINT, Print, print, EXIT, Exit, exit};
+
+		printMenu();
+
+		getline(cin, argLine);
+
+		switch(arg[0]){
+
+		case INSERT:
+		case Insert:
+		case insert:	insert(b_List);
+				break;
+		case LEND:
+		case Lend:
+		case lend:	lend(b_List);
+				break;
+		case SAVE:
+		case Save:
+		case save:	save(arg[1]);
+				break;
+		case RETURNED:
+		case Returned:
+		case returned:	returned(b_List);
+				break;
+		case PASSDAY:
+		case Passday:
+		case passday:	passDay(b_List);
+				break;
+		case PRINT:
+		case Print:
+		case print:	print(b_List);
+				break;
+		case EXIT:
+		case Exit:
+		case exit:	setExit = getExit();
+				break;
+		}
+		
 	}
 
 	return 0;
 }
 
+void printMenu(){
+	
+	cout << "  =============== Available Commend & Format ===============" << endl << endl;
+	cout << "  1. INSERT BookTitle; Author; PubYear; Edition" << endl;
+	cout << "  2. LEND BookTitle; Person Borrowing; How many days" << endl;
+	cout << "  3. SAVE new_filename.txt" << endl;
+	cout << "  4. RETURNED BookTitle" << endl;
+	cout << "  5. PASSDAY" << endl;
+	cout << "  6. PRINT" << endl;
+	cout << "  7. EXIT" << endl << endl;
+	cout << "  ==========================================================" << endl;
+	cout << "  >>";
+}
+
 //TODO: THREE - function Load
 
-book *load(){
+book *load(string FileName){
 	//open file and store list
 	book[50] bookList;
+
+	int i = 0;
+
+	ifstream inData;
+	inData.open(FileName.c_str())
+
+	if(!inData){
+		
+		cout << FileName << " does not exist." << endl;
+		
+	}
+	else{
+
+		while(!inData.eof()){
+		
+			inData >> bookList[i].title >> bookList[i].p_Year >> bookList[i].author >> bookList[i].edition >> bookList[i].b_Person >> bookList[i].l_Day;
+		
+			i++;
+
+		}
+
+		print(bookList);
+	}
+
+	inData.cloase();
 
 	return bookList;
 
@@ -62,6 +144,15 @@ void save(string fileName){
 //TODO: THREE - function Print
 void print(book[] bookList){
 
+	cout << "  ======================================== Book Catalog ========================================" << endl;
+	cout << setw(8) << "Title" << setw(24) << "Author" << setw(28) << "Publised Year" << setw(10) << "Edition" << setw(10) << "Borrower" << setw(16) << "Days Borrowed" << endl;
+
+	for(int i = 0; i < bookList.size() ; i++ ){
+
+		cout << "   " << bookList[i].title << setw(24 - bookList[i].title.length()) << bookList[i].author << setw(28 - bookList[i].author.length()) << bookList[i].p_Year << setw(10 - bookList[i].p_Year.length()) << bookList[i].edition << setw(10 - bookList[i].edition.length()) << bookList[i].b_Person << setw(16 - bookList[i].b_Person.length()) << bookList[i].l_Day << endl;
+	}
+
+	cout << "  ============================================= End =============================================" << endl;
 
 }
 
